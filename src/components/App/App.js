20a686ad8ca557caf5741f5eae613c42884c8b87
Nelson Bozzelli <nrbozzelli@gmail.com>
2019-08-26 21:36:29 -0600

    Add searchBar component functionality.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,14 +26,18 @@ const businesses = [
 ];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, and ${sortBy}.`);
+  }
+  
   render() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} />
-    </div>   
-  );
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>   
+    );
   }
 }
 
